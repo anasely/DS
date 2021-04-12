a72def2598026b07080c6f256cae7a1f6fed4cae
anas <anas.abbal13@gmail.com>
2021-04-12 21:25:20 +0000

    $

---
+++ Array/merge_two_array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+    int arr1[10], arr2[10], arr3[20];
+    int m1, m2, s;
+    int i = 0, j = 0, k = 0;
+
+    std::cout << "\nEnter number of elements in array 1: ";
+    std::cin >> m1;
+    std::cout << "\nEnter element array 1: ";
+    for(i = 0; i < m1; i++)
+        std::cin >> arr1[i];
+
+    std::cout << "\nEnter number of element in array 2: ";
+    std::cin >> m2;
+    for(s = 0; s < m2; s++)
+        std::cin >> arr2[s];
+    
+    while(i < m1 && j < m2){
+        
+    }
+}
