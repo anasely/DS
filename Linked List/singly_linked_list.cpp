bfc909205b1e6d4a9b239e585c1ce1934648cb14
anas <anas.abbal13@gmail.com>
2021-04-12 22:42:25 +0000

    $

---
+++ Linked List/singly_linked_list.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+
+struct Node{
+    int info;
+    struct Node *next;
+}*start;
+
+class Singlylist{
+public:
+    Singlylist(){
+        start = NULL;
+    }
+    Node *create_list(int);
+    void inserition_at_beginning();
+    void insertion_at_position();
+    void insertion_at_end();
+    void deletion_from_position();
+    void display();
+};
+
+
+int main(){
+    Singlylist obj;
+    int choice;
+
+    std::cout << "\n *** MENU ***";
+    std::cout << "\n1. Insertion at beginnig";
+    std::cout << "\2. Insertion at specified position";
+    std::cout << "\n3. Insertion at the end";
+    std::cout << "\n4. Deletion from spec position";
+    std::cout << "\n5. Display";
+    std::cout << "\n6. Exit";
+    std::cin >> choice;
+    switch (choice){
+        case 1:
+            obj.inserition_at_beginning();
+            break;
+        case 2:
+            obj.insertion_at_position();
+            break;
+        case 3:
+            obj.insertion_at_end();
+            break;
+        case 4:
+            obj.deletion_from_position();
+            break;
+        case 5:
+            obj.display();
+            break;
+        case 6:
+            exit(0);
+            break;
+        default:
+            std::cout << "Wrong choice";
+            exit(0);
+    }
+    return 0;
+}
