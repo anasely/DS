a947f64f5f4cdeb5fc0d6858525228699aa13791
anas <anas.abbal13@gmail.com>
2021-04-13 00:39:50 +0000

    $

--- Linked List/circular_list.cpp
+++ Linked List/circular_list.cpp
@@ -17,7 +17,44 @@ public:
     void deletion();
     void display();
 };
+void CircularList::create_list(){
+    struct Node *ptr;
+    int item;
+    
+    ptr = new struct Node;
+    
+    std::cout << "\nEnter value of node: ";
+    std::cin >> item;
 
+    ptr->data = item;
+
+    if(end == NULL){
+        end = ptr;
+        ptr->next = end;
+    }
+    else{
+        ptr->next = end->next;
+        end->next = pre;
+        end = ptr;
+    }
+    std::cout << "\nList created Successfully!!";
+}
+void CircularList::insert_at_beginning(){
+    struct Node *ptr;
+    int item;
+
+    std::cout << "\nEnter value of node: ";
+    std::cin >> item;
+
+    if(end == NULL){
+        std::cout << "\List is not created";
+        return;
+    }
+    else{
+        ptr = new struct Node;
+        
+    }
+}
 int main(){
     CircularList obj;
     int choice;
